61af1340aa540c0689954c8697a5bedc2b33009f
nbalcarc <nathan.balcarcel@wsu.edu>
2024-03-27 23:27:24 +0000

    Simplify code

--- src/main.py
+++ src/main.py
@@ -15,32 +15,19 @@ class Finger:
 class Hands:
     """Contains a set of fingers and their assigned keys on a keyboard."""
     def __init__(self, hand_placements: npt.NDArray):
-        flattened = reduce(lambda a, x: a + list(x), hand_placements, [])
+        flattened = reduce(lambda a, x: a + list(x), hand_placements, []) #2D -> 1D
         uniques = set(list(flattened)) #which fingers there are
-        fingers = dict(map(lambda x: (x, Finger()), uniques))
-
-        self.fingermap = dict()
-        for i, h in enumerate(flattened):
-            self.fingermap[i] = fingers[h]
-
-
-def thing(layout: np.chararray):
-    pass
+        fingers = dict(map(lambda x: (x, Finger()), uniques)) #assign a Finger to an ID
+        self.fingermap = {i: fingers[h] for (i, h) in enumerate(flattened)} #assign keys to Fingers
 
 
 def main():
     """Main entry point."""
 
-    layout = np.chararray((3, 10))
-
     Hands(config.home_row)
-
-
-    
 
 
 if __name__ == "__main__":
     main()
-
 
 
