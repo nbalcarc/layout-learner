b8b29573b8e6d8a70a28533d045ce29f33d45406
Aashik Sharif <ashiktcy.s@gmail.com>
2024-04-22 06:28:29 -0700

    Update DQN.py

--- src/DQN.py
+++ src/DQN.py
@@ -1,33 +1,95 @@
+''''
+The DQN class defines the neural network architecture.
+The ReplayBuffer class is used to store and sample experiences for training.
+The DQNAgent class implements the DQN algorithm, including the training procedure and experience replay.
+The agent interacts with the environment, collects experiences, and updates the neural network parameters to learn the Q-values.
+''''
 import numpy as np
 import tensorflow as tf
+from collections import deque
 
 # Define the neural network architecture
-class QNetwork(tf.keras.Model):
+class DQN(tf.keras.Model):
     def __init__(self, num_actions):
-        super(QNetwork, self).__init__()
+        super(DQN, self).__init__()
         self.dense1 = tf.keras.layers.Dense(64, activation='relu')
         self.dense2 = tf.keras.layers.Dense(64, activation='relu')
-        self.output_layer = tf.keras.layers.Dense(num_actions)
+        self.dense3 = tf.keras.layers.Dense(num_actions)
 
     def call(self, state):
         x = self.dense1(state)
         x = self.dense2(x)
-        q_values = self.output_layer(x)
-        return q_values
+        return self.dense3(x)
 
-# Define the DQN agent
+# Define the replay buffer
+class ReplayBuffer:
+    def __init__(self, max_size):
+        self.buffer = deque(maxlen=max_size)
+
+    def add(self, experience):
+        self.buffer.append(experience)
+
+    def sample(self, batch_size):
+        idx = np.random.choice(len(self.buffer), batch_size, replace=False)
+        return [self.buffer[i] for i in idx]
+
+# Define the Deep Q-Network algorithm
 class DQNAgent:
-    def __init__(self, num_actions):
+    def __init__(self, num_actions, state_dim):
         self.num_actions = num_actions
-        self.q_network = QNetwork(num_actions)
+        self.state_dim = state_dim
+        self.model = DQN(num_actions)
+        self.target_model = DQN(num_actions)
+        self.target_model.set_weights(self.model.get_weights())
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-        self.loss_function = tf.keras.losses.MeanSquaredError()
+        self.replay_buffer = ReplayBuffer(max_size=10000)
+        self.gamma = 0.99
+        self.epsilon = 1.0
+        self.epsilon_decay = 0.999
+        self.epsilon_min = 0.01
+        self.batch_size = 32
 
-    def select_action(self, state, epsilon):
-        if np.random.rand() < epsilon:
-            return np.random.randint(self.num_actions)
+    def act(self, state):
+        if np.random.rand() < self.epsilon:
+            return np.random.choice(self.num_actions)
         else:
-            q_values = self.q_network(tf.expand_dims(state, 0))
-            return np.argmax(q_values.numpy()[0])
+            q_values = self.model.predict(np.array([state]))
+            return np.argmax(q_values[0])
 
-    def train(self, states, actions, next_states, rewards, don
+    def update_target_model(self):
+        self.target_model.set_weights(self.model.get_weights())
+
+    def train(self):
+        if len(self.replay_buffer.buffer) < self.batch_size:
+            return
+
+        minibatch = self.replay_buffer.sample(self.batch_size)
+        states = np.array([experience[0] for experience in minibatch])
+        actions = np.array([experience[1] for experience in minibatch])
+        rewards = np.array([experience[2] for experience in minibatch])
+        next_states = np.array([experience[3] for experience in minibatch])
+        dones = np.array([experience[4] for experience in minibatch])
+
+        target = rewards + (1 - dones) * self.gamma * np.amax(self.target_model.predict(next_states), axis=1)
+        target_f = self.model.predict(states)
+        target_f[np.arange(self.batch_size), actions] = target
+
+        self.model.train_on_batch(states, target_f)
+
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+# Initialize the DQN agent
+agent = DQNAgent(num_actions, state_dim)
+
+# Train the DQN agent
+for episode in range(num_episodes):
+    state = initial_state
+    done = False
+    while not done:
+        action = agent.act(state)
+        next_state, reward, done = env.step(action)
+        agent.replay_buffer.add((state, action, reward, next_state, done))
+        state = next_state
+        agent.train()
+    agent.update_target_model()
